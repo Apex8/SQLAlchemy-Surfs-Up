eb1943d2aadd6e06e398fe64c0ba2df8bac28360
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-08-24 14:29:43 -0400

    restart edit

--- app.py
+++ app.py
@@ -26,108 +26,39 @@ session = Session(engine)
 # Setup Flask
 app = Flask(__name__)
 
-latestDate = (session.query(Measurement.date)
-                .order_by(Measurement.date.desc())
-                .first())
-latestDate = list(np.ravel(latestDate))[0]
-
-latestDate = dt.datetime.strptime(latestDate, '%Y-%m-%d')
-latestYear = int(dt.datetime.strftime(latestDate, '%Y'))
-latestMonth = int(dt.datetime.strftime(latestDate, '%m'))
-latestDay = int(dt.datetime.strftime(latestDate, '%d'))
-
-yearBefore = dt.date(latestYear, latestMonth, latestDay) - dt.timedelta(days=365)
-yearBefore = dt.datetime.strftime(yearBefore, '%Y-%m-%d')
-
-# Setup Flask (@app) Routes
-
+# Setup Flask Routes
 
 @app.route("/")
 def main():
     """List all routes that are available."""
     return (
-        f"Surf's Up, it's vacation time!: Hawai'i Climate API<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/Precipitation<br/>"
-        f"/api/v1.0/Stations<br/>"
-        f"/api/v1.0/Temperature<br/>"
-        f"/api/v1.0/StartDate<br/>"
-        f"/api/v1.0/StartDateEndDate<end>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/temperature<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
     )
 
-
-@app.route("/api/v1.0/Precipitaton")
+@app.route("/api/v1.0/precipitation")
 def precipitation():
-    results = (session.query(Measurement.date, Measurement.prcp, Measurement.station)
-                      .filter(Measurement.date > yearBefore)
-                      .order_by(Measurement.date)
-                      .all())
-    
-    precipData = []
-    for result in results:
-        precipDict = {result.date: result.prcp, "Station": result.station}
-        precipData.append(precipDict)
-    return jsonify(precipData)
+    """Return the JSON representation of your dictionary."""
 
+    print("Received precipitation api request.")
 
-@app.route("/api/v1.0/Stations")
-def stations():
-    results = session.query(Station.name).all()
-    all_stations = list(np.ravel(results))
-    return jsonify(all_stations)
-
-
-@app.route("/api/v1.0/Temperature")
-def temperature():
-    results = (session.query(Measurement.date, Measurement.tobs, Measurement.station)
-                      .filter(Measurement.date > yearBefore)
-                      .order_by(Measurement.date)
-                      .all())
-
-    tempData = []
-    for result in results:
-        tempDict = {result.date: result.tobs, "Station": result.station}
-        tempData.append(tempDict)
-    return jsonify(tempData)
-
-@app.route("/api/v1.0/StartDate")
-def StartDate(start):
-    print("Received only start date API request.")
-
-    # Last date in the database
+    # Query for the first and final dates
     final_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
-    max_date = final_date_query[0][0]
+    max_date_string = final_date_query[0][0]
+    max_date = dt.datetime.strptime(max_date_string, "%Y-%m-%d")
+    begin_date = max_date - dt.timedelta(365)
 
-    # Calculate the temperatures
-    temps = Measurement(start, max_date)
+    # Query for all the precipitation data for all the dates
+    precipitation_data = session.query(func.strftime("%Y-%m-%d", Measurement.date), Measurement.prcp).\
+        filter(func.strftime("%Y-%m-%d", Measurement.date) >= begin_date).all()
+    
 
-    # Now create a list of dictionaries
-    start_list = []
-    dates_dict = {'start_date': start, 'end_date': max_date}
-    start_list.append(dates_dict)
-    start_list.append({'Observation': 'TMIN', 'Temperature': temps[0][0]})
-    start_list.append({'Observation': 'TAVG', 'Temperature': temps[0][1]})
-    start_list.append({'Observation': 'TMAX', 'Temperature': temps[0][2]})
+    results_dict = {}
+    for result in precipitation_data:
+        results_dict[result[0]] = result[1]
 
-    return jsonify(start_list)
-
-@app.route("/api/v1.0/StartDateEndDate")
-def StartDateEndDate(start, end):
-    """Return a JSON list for a given start or start-end range."""
-    print("Received start date and end date API request.")
-
-    # Calculate the temperatures
-    temps = Measurement(start, end)
-
-    # Now create a list of dictionaries
-    return_list = []
-    date_dict = {'start_date': start, 'end_date': end}
-    return_list.append(date_dict)
-    return_list.append({'Observation': 'TMIN', 'Temperature': temps[0][0]})
-    return_list.append({'Observation': 'TAVG', 'Temperature': temps[0][1]})
-    return_list.append({'Observation': 'TMAX', 'Temperature': temps[0][2]})
-
-    return jsonify(return_list)
-
-if __name__ == "__main__":
-    app.run(debug = True)
+    return jsonify(results_dict)
